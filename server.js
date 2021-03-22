4514613aefe67a878877b781eb4290de308a38a1
“adelina-ok” <adelina.ceja@seattlecolleges.edu>
2021-03-22 14:50:21 -0700

    adds user data to server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var data = require('./data/test.json');
 
 var app = express();
 const PORT = 3000;
@@ -8,9 +9,10 @@ app.set('view engine','ejs');//sent views to EJS
 //this is how a route is built in Express
 app.get('/', (req,res)=>{
   var title = "Our Home Page";
-  res.render('index',{title:title});
+  res.render('pages/index',{title:title});
 });
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data);
 });
